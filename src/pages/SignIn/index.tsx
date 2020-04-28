1c2116e38a6e1ff2c149b71ecc0c98490f3c0fc9
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-04-27 23:09:21 -0300

    Input e Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Image } from 'react-native';
+import logoImg from '../../assets/logo.png';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Title } from './styles';
-
-import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faca seu login</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => console.log('Works!')}>Entrar</Button>
     </Container>
   );
 };
