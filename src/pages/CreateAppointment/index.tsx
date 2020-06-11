6b549d2cca727e7e6017c3d1f83952a1fd49a15d
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-11 19:52:40 -0300

    Estrutura da criacao

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback } from 'react';
+
+import { useRoute, useNavigation } from '@react-navigation/native';
+
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
 
 import { useAuth } from '../../hooks/auth';
 
+interface RouteParams {
+  providerId: string;
+}
+
 const CreateAppointment: React.FC = () => {
-  const { signOut } = useAuth();
+  const { user } = useAuth();
+  const route = useRoute();
+
+  const { providerId } = route.params as RouteParams;
+
+  const { goBack } = useNavigation();
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar
+          source={{
+            uri:
+              user.avatar_url || `https://api.adorable.io/avatars/${user.id}`,
+          }}
+        />
+      </Header>
+    </Container>
   );
 };
 
