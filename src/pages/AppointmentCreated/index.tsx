f8d241d9a0e9ab46bae0fd0c12c0e1f4973a84ef
Nathalia Cristina Gomes Oliveira <nathaliacristina20@hotmail.com>
2020-06-11 23:22:45 -0300

    Formatando a data

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
-
-import { useNavigation } from '@react-navigation/native';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import {
   Container,
   Title,
@@ -10,8 +11,23 @@ import {
   OkButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+
+  const { params } = useRoute();
+  const routeParams = params as RouteParams;
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
+
   const handleOkPress = useCallback(() => {
     reset({
       routes: [
@@ -26,7 +42,7 @@ const AppointmentCreated: React.FC = () => {
     <Container style={{ flex: 1, justifyContent: 'center' }}>
       <Icon name="check" size={80} color="#04d361" />
       <Title>Agendamento concluído</Title>
-      <Description>dasdsa</Description>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPress}>
         <OkButtonText>Ok</OkButtonText>
